04671b9e9c265bcf63ad5226f8ceffd272881d4d
Kenny <kt.tian@gmail.com>
2019-11-27 08:41:19 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,11 @@
 declare module 'react-native-keyevent';
+
+export type KeyEventProps = { keyCode: number; action: string; pressedKey?: string };
+
+export function onKeyDownListener(keyEvent: any): void;
+export function onKeyUpListener(keyEvent: any): void;
+export function onKeyMultipleListener(keyEvent: any): void;
+
+export function removeKeyDownListener(): void;
+export function removeKeyUpListener(): void;
+export function removeKeyMultipleListener(): void;
